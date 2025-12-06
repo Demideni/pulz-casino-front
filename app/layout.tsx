362d57580e8fe39576fc5f030810f411a39380c5
Demideni <demidovden@yahoo.com>
2025-12-05 16:02:55 -0800

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               <img
                 src="/pulz-logo-light.PNG"
                 alt="Pulz Casino"
-                className="h-6 w-auto"
+                className="h-10 w-84"
               />
             </Link>
 
